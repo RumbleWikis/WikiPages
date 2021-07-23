bb2927ac2f9d1dee1d62486c681b1dae73652b77
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-07-23 16:10:14 -0700

    start v0.2

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -2,46 +2,57 @@ import { mwn } from "mwn";
 import * as fs from "fs";
 import getAllFiles from "../utils/getAllFiles";
 import md5Hash from "../utils/md5Hash";
-import type { ClientOptions, WPFile } from "../types";
+import { Evt, to } from "evt";
 import { basename, dirname, extname } from "path";
+import type { ClientOptions, WPFile } from "../types";
 
 /**
  * The WikiPages Client.
  */
-export class Client {
+export class Client extends Evt<
+["ready", undefined] | 
+["runningStarted", undefined] | 
+["runningEnded", undefined] |
+["editError", WPFile, any] |
+["createError", WPFile, any]
+> {
   /**
-   * Options for the Client, this can not be changed after initialization.
+   * Client options for the client, this can be changed later **when** the client is not running.
    */
   public clientOptions?: ClientOptions;
   /**
-   * The initialization state.
+   * The initialization state, initially `false` and will become `true` when the **mwnClient** is ready.
+   * 
+   * Use `client.$attach(to("ready"), () => {...})` to call a function when the client is ready.
    */
   public initialized = false;
   /**
-   * Whether or not there is a currently running instance scanning all files.
+   * The value of whether or not the client is currently running.
+   * 
+   * Use `client.$attach(to("runningStarted"), () => {...})` to call a function when the client started running, or `client.$attach(to("runningStopped"), () => {...})` when the client stopped running.
    */
   public running = false;
   /**
-   * The mwn client for requests.
+   * The API client **mwn** for doing API requests to the MediaWiki site from `apiUrl`.
+   * 
+   * There is currently no Deno variant available, which is why this library is still written in NodeJS.
    */
   private mwnClient?: mwn;
   /**
-   * Construct a new Client.
+   * Create a new WikiPages client.
    * ```
    * const instance = new Client({
-   *   apiUrl: "https://en.wikipedia.org/api.php",
-   *   username: "TotallyNotBot",
-   *   password: "Password123",
+   *   username: "Username",
+   *   password: "Password",
    *   srcDirectory: "src/",
-   *   cacheFile: "cache/",
-   *   onReady: (client) => {
-   *     console.log("Ready")
-   *   }
+   *   cacheFile: "cache.json",
+   *   apiUrl: "https://en.wikipedia.org/api.php",
    * })
    * ```
-   * @param options - The client options.
+   * @param options - The client options, note that this can be changed later with `client.setSettings()`.
    */
   constructor(options: ClientOptions) {
+    super()
     if (fs.existsSync(options.cacheFile) ? !fs.lstatSync(options.cacheFile).isDirectory() : true) {
       this.clientOptions = this.clientOptions;
       mwn.init({
@@ -49,24 +60,26 @@ export class Client {
         maxRetries: options.maxRetries,
         username: options.username,
         password: options.password,
-        userAgent: `${options.userAgent || "Instance"} (powered by @rumblewikis/wikipages)`
+        userAgent: `${options.userAgent ?? "Instance"} (powered by @rumblewikis/wikipages)`
       }).then((client) => {
         this.initialized = true;
         this.clientOptions = options;
         this.mwnClient = client;
-        try { if (options.onReady) options.onReady(this) } catch {};
+        this.post(["ready", undefined]);
       });
     } else throw new Error(`"${options.cacheFile}" is not a valid dirrectory for "cacheFile"`)
   }
 
   /**
-   * Run the bot, going through all middlewares.
-   * @param commitComment - The default message to commit with
+   * Run the bot, going through all middlewares, and then pushing the files with `shouldCommit` as true to the site.
+   * @param commitComment - The default message to commit with, can be changed by the middlewares.
    */
   public run(commitComment: string): Promise<void> {
     // i hate every part of this - Jullian 7/17/21
+    if (!fs.existsSync(this.clientOptions!.srcDirectory)) throw new Error("Could not run because `srcDirectory` doesn't exist."); 
     if (!this.initialized || this.running) throw new Error(`Could not run because it was not initialized or was already running.`);
     return new Promise((resolve) => {
+      this.post(["runningStarted", undefined]);
       this.running = true;
       const md5Hashes: Map<string, string> = new Map<string, string>();
       const newMd5Hashes: Map<string, string> = new Map<string, string>();
@@ -105,16 +118,20 @@ export class Client {
           folder = basename(folderDirectory);
         }
 
+        // TODO: WPFile class
         let wpFile: WPFile = { 
           longExtension,
+          originalLongExtension: longExtension,
           shortExtension,
+          originalShortExtension: shortExtension,
+          commitComment,
           source: content.toString(),
-          originalDirectory: file,
-          commitComment: commitComment,
+          originalPath: file,
           shouldCommit: true,
           path:`${namespace}${folderDirectory === "." ? "" : `${folderDirectory}/`}${fileName}`
         };
 
+        // Change bruh.doc.wikitext to bruh/doc
         if (wpFile.longExtension === ".doc.wikitext") wpFile.path = `${wpFile.path}/doc`;
         if (wpFile.shortExtension === ".css" || wpFile.shortExtension === ".js") wpFile.path = `${wpFile.path}${wpFile.longExtension}`;
 
@@ -127,32 +144,34 @@ export class Client {
           }
         
         if (wpFile.shouldCommit) pagesToEdit.set(wpFile.path, wpFile);
-        
-        const allEdits: Promise<unknown>[] = [];
-        pagesToEdit.forEach((file) => {
-          if (!(md5Hashes.get(file.path) && md5Hash(file.source.trimEnd()) === md5Hashes.get(file.path)))
-            allEdits.push(new Promise((resolve) => {
-              setTimeout(() => {
-                this.mwnClient!.edit(file.path, (revision) => {
-                  if (!(md5Hashes.get(file.path) && md5Hash(revision.content.trimEnd()) === md5Hash(file.source.trimEnd()))) {
-                    newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
-                    return {
-                      summary: file.commitComment,
-                      text: file.source
-                    }
-                  } else return {};
-                 })
-                 .then(resolve)
-                 .catch((error) => {
-                   if (error.code === "missingtitle") {
-                     newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
-                    this.mwnClient!.create(file.path, file.source, file.commitComment).then(resolve).catch();
-                   }
-                 });
-              }, (allEdits.length + 1) * 10000)
-            }));
-          });
-  
+      });
+
+      const allEdits: Promise<unknown>[] = [];
+      pagesToEdit.forEach((file) => {
+        if (!(md5Hashes.get(file.path) && (md5Hash(file.source.trimEnd()) === md5Hashes.get(file.path))))
+          allEdits.push(new Promise((resolve) => {
+            setTimeout(() => {
+              this.mwnClient!.edit(file.path, (revision) => {
+                if (!(md5Hashes.get(file.path) && md5Hash(revision.content.trimEnd()) === md5Hash(file.source.trimEnd()))) {
+                  newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
+                  return {
+                    summary: file.commitComment,
+                    text: file.source
+                  }
+                } else return {};
+              })
+              .then(resolve)
+              .catch((error) => {
+                if (error.code === "missingtitle") {
+                  newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
+                  this.mwnClient!.create(file.path, file.source, file.commitComment).then(resolve).catch((error) => {
+                    this.post(["createError", file, error]);
+                  });
+                } else this.post(["editError", file, error])
+              });
+            }, (allEdits.length + 1) * 10000)
+          }));
+        });
         Promise.all(allEdits).then(() => {
           const newMd5HashesJSON: Record<string, string> = {};
           newMd5Hashes.forEach((value, key) => {
@@ -161,9 +180,9 @@ export class Client {
     
           fs.writeFileSync(this.clientOptions!.cacheFile, JSON.stringify(newMd5HashesJSON));
           this.running = false;
+          this.post(["runningEnded", undefined]);
           resolve(); 
         });
-      });
     })
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,78 +1,91 @@
 import { Client } from "./classes/Client";
 
 /**
- * A file interface for WikiPages.
+ * A file to be handled by client middlewares.
  */
 export interface WPFile {
   /**
-   * The original directory of the file.
+   * The original path of where the file was located.
    */
-  readonly originalDirectory: string;
+  readonly originalPath: string;
   /**
-   * The short extension of the file, ex `.lua`, not including `.client.lua`
+   * The short extension of the file, ex `.lua`, not including `.client.lua`.
+   * 
+   * See `originalShortExtension` for the original short extension.
    */
   shortExtension: string;
   /**
+   * The original short extension of the original file, ex `.lua`, not including `.client.lua`.
+   */
+  readonly originalShortExtension: string;
+  /**
    * The long extension o fthe file, ex `.client.lua`, not including **just** `.lua`
+   * 
+   * See `originalLongExtension` for the original long extension.
    */
   longExtension: string;
+  /*
+   * The original long extension of the original file, ex `.lua`, not including `.client.lua`.
+   */
+   readonly originalLongExtension: string;
   /**
-   * The comment for the MediaWiki commit
+   * The default (or modified) comment for the MediaWiki commit, "edit comment".
    */
   commitComment: string;
   /**
-   * Whether or not it should commit when it goes through all middlewares
+   * Whether or not it should be committed to the site after finished going through the middlewares.
    */
   shouldCommit: boolean;
   /**
-   * THe new MediaWiki path of the file.
+   * THe MediaWiki path of where the page would be. (MediaWiki/Common.js -> MediaWiki:Common.js)
    */
   path: string;
   /**
-   * The source string of the file.
+   * The source string of the file, can be modified.
    */
   source: string;
 }
 
 /**
- * Middleware that will be called on every file found.
+ * Middleware that will be called on every file in the `srcDirectory`.
  */
 export interface Middleware {
   /**
-   * The RegExp/string to match in the short extension,  ex `.lua`, not including `.client.lua`
+   * The RegExp/string to match in the short extension,  ex `.lua`, not including `.client.lua`.
    */
   matchShortExtension?: RegExp | string;
   /**
-   * The RegExp/string to match in the long extension, ex `.client.lua`, not including **just** `.lua`
+   * The RegExp/string to match in the long extension, ex `.client.lua`, not including **just** `.lua`.
    */
   matchLongExtension?: RegExp | string;
   /**
-   * The Regexp/string to match in the path of the file
+   * The Regexp/string to match in the path of the file.
    */
   matchPath?: RegExp | string;
   /**
-   * The settings index to be indexed with
+   * The settings index to index `middlewareSettings` with.
    */
   settingsIndex?: string;
   /**
-   * The execute function of the middleware
+   * The execute function of the middleware.
    */
   execute: (file: WPFile, settings?: Record<string, unknown>) => WPFile
 }
+
 /**
- * Options for the Client, this can not be changed after initialization.
+ * Options for the Client, this can be changed with `client.setSettings()`.
  */
 export interface ClientOptions {
   /**
-   * The username for the account, ex: `TotallyNotBot`, can also be a username from [Special:BotPasswords](https://www.mediawiki.org/wiki/Manual:Bot_passwords)
+   * The username for the account, ex: `TotallyNotBot`, can also be a username from [Special:BotPasswords](https://www.mediawiki.org/wiki/Manual:Bot_passwords).
    */
   username: string;
   /**
-   * The password for the account, ex: `Password123`, can also be a password from [Special:BotPasswords](https://www.mediawiki.org/wiki/Manual:Bot_passwords)
+   * The password for the account, ex: `Password123`, can also be a password from [Special:BotPasswords](https://www.mediawiki.org/wiki/Manual:Bot_passwords).
    */
   password: string;
   /**
-   * The API url for the MediaWiki wiki, add /api.php to the base URL of the site, ex: `https://en.wikipedia.org/api.php`
+   * The API url for the MediaWiki wiki, add /api.php to the base URL of the site, ex: `https://en.wikipedia.org/api.php`.
    */
   apiUrl: string;
   /**
@@ -84,33 +97,23 @@ export interface ClientOptions {
    */
   cacheFile: string;
   /**
-   * The user agent for all requests.
-   * 
-   * This is a feature from **mwn**
+   * The user agent for all requests, use this to identify where the requests originate from.
    */
   userAgent?: string;
   /**
-   * Middleware settings for middleware, see https://github.com/RumbleWikis/WikiPages#Middlewares for more info
+   * Middleware settings for middleware, see https://github.com/RumbleWikis/WikiPages#Middlewares for more info.
    */
   middlewareSettings?: Record<string, Record<string, any>>;
   /**
-   * The maximum allowed of retries, will quit after the amount of retries.
-   * 
-   * This is a feature from **mwn**
+   * The maximum allowed of retries, will quit trying after the maximum allowed retries.
    */
   maxRetries?: number;
   /**
-   * The main namespace, `(main)` on MediaWiki.
-   * 
-   * Default `Main`
+   * The main namespace, `(main)` on MediaWiki, default `Main`.
    */
   mainNamespace?: string;
   /**
-   * The middleware to add, see https://github.com/RumbleWikis/WikiPages#Middlewares for more info.
+   * The array middlewares to add, see https://github.com/RumbleWikis/WikiPages#Middlewares for more info.
    */
   middlwares?: Middleware[];
-  /**
-   * Start event to be called when ready.
-   */
-  onReady?: (client: Client) => void;
 }
