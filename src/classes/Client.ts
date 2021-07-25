6389049626b2c33131c0a10efe0fc784f9b8d139
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-07-25 10:11:18 -0700

    getters

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -19,25 +19,34 @@ export class Client extends Evt<
   /**
    * Client options for the client, this can be changed later **when** the client is not running.
    */
-  public clientOptions?: ClientOptions;
+  private _clientOptions?: ClientOptions;
+  public get clientOptions() {
+    return this._clientOptions;
+  }
   /**
    * The initialization state, initially `false` and will become `true` when the **mwnClient** is ready.
    * 
    * Use `client.$attach(to("ready"), () => {...})` to call a function when the client is ready.
    */
-  public initialized = false;
+  private _initialized = false;
+  public get initialized() {
+    return this._initialized;
+  }
   /**
    * The value of whether or not the client is currently running.
    * 
    * Use `client.$attach(to("runningStarted"), () => {...})` to call a function when the client started running, or `client.$attach(to("runningStopped"), () => {...})` when the client stopped running.
    */
-  public running = false;
+  private _running = false;
+  public get running() {
+    return this._running;
+  }
   /**
    * The API client **mwn** for doing API requests to the MediaWiki site from `apiUrl`.
    * 
    * There is currently no Deno variant available, which is why this library is still written in NodeJS.
    */
-  private mwnClient?: mwn;
+  private _mwnClient?: mwn;
   /**
    * Create a new WikiPages client.
    * ```
@@ -54,7 +63,7 @@ export class Client extends Evt<
   constructor(options: ClientOptions) {
     super()
     if (fs.existsSync(options.cacheFile) ? !fs.lstatSync(options.cacheFile).isDirectory() : true) {
-      this.clientOptions = this.clientOptions;
+      this._clientOptions = options;
       mwn.init({
         apiUrl: options.apiUrl,
         maxRetries: options.maxRetries,
@@ -62,9 +71,9 @@ export class Client extends Evt<
         password: options.password,
         userAgent: `${options.userAgent ?? "Instance"} (powered by @rumblewikis/wikipages)`
       }).then((client) => {
-        this.initialized = true;
-        this.clientOptions = options;
-        this.mwnClient = client;
+        this._initialized = true;
+        this._clientOptions = options;
+        this._mwnClient = client;
         this.post(["ready", undefined]);
       });
     } else throw new Error(`"${options.cacheFile}" is not a valid dirrectory for "cacheFile"`)
@@ -76,15 +85,15 @@ export class Client extends Evt<
    */
   public run(commitComment: string): Promise<void> {
     // i hate every part of this - Jullian 7/17/21
-    if (!fs.existsSync(this.clientOptions!.srcDirectory)) throw new Error("Could not run because `srcDirectory` doesn't exist."); 
-    if (!this.initialized || this.running) throw new Error(`Could not run because it was not initialized or was already running.`);
+    if (!fs.existsSync(this._clientOptions!.srcDirectory)) throw new Error("Could not run because `srcDirectory` doesn't exist."); 
+    if (!this._initialized || this._running) throw new Error(`Could not run because it was not initialized or was already running.`);
     return new Promise((resolve) => {
       this.post(["runningStarted", undefined]);
-      this.running = true;
+      this._running = true;
       const md5Hashes: Map<string, string> = new Map<string, string>();
       const newMd5Hashes: Map<string, string> = new Map<string, string>();
       try { 
-        const hashJSON = JSON.parse(fs.readFileSync(this.clientOptions!.cacheFile).toString());
+        const hashJSON = JSON.parse(fs.readFileSync(this._clientOptions!.cacheFile).toString());
         // we can assume it didn't error, so continue
         for (const [file, hash] of Object.entries<string>(hashJSON))
           md5Hashes.set(file, hash);
@@ -92,17 +101,17 @@ export class Client extends Evt<
 
 
       const pagesToEdit = new Map<string, WPFile>();
-      const files = getAllFiles(this.clientOptions!.srcDirectory);
+      const files = getAllFiles(this._clientOptions!.srcDirectory);
 
       files.forEach(file => {
         const content = fs.readFileSync(file);
-        let shortFileDirectory: string = file.slice(this.clientOptions!.srcDirectory.length + 1);
+        let shortFileDirectory: string = file.slice(this._clientOptions!.srcDirectory.length + 1);
         const shortFileDirectorySplit = shortFileDirectory.split("/");
         // @ts-ignore: This will never be undefined
         let namespace: string = shortFileDirectorySplit.shift();
         shortFileDirectory = shortFileDirectorySplit.join("/");
 
-        namespace = ((namespace === this.clientOptions!.mainNamespace) ? "" : `${namespace}:`);
+        namespace = ((namespace === this._clientOptions!.mainNamespace) ? "" : `${namespace}:`);
 
         let folderDirectory: string = dirname(shortFileDirectory);
         let folder: string = basename(folderDirectory);
@@ -135,12 +144,12 @@ export class Client extends Evt<
         if (wpFile.longExtension === ".doc.wikitext") wpFile.path = `${wpFile.path}/doc`;
         if (wpFile.shortExtension === ".css" || wpFile.shortExtension === ".js") wpFile.path = `${wpFile.path}${wpFile.longExtension}`;
 
-        if (this.clientOptions!.middlwares)
-          for (const middleware of this.clientOptions!.middlwares) {
+        if (this._clientOptions!.middlwares)
+          for (const middleware of this._clientOptions!.middlwares) {
             if ((middleware.matchLongExtension ? longExtension.match(middleware.matchLongExtension) : true) 
               && (middleware.matchShortExtension ? shortExtension.match(middleware.matchShortExtension) : true) 
               && (middleware.matchPath ? wpFile.path.match(middleware.matchPath) : true)) 
-                wpFile = middleware.execute(wpFile, middleware.settingsIndex ? this.clientOptions!.middlewareSettings?.[middleware.settingsIndex] : undefined);
+                wpFile = middleware.execute(wpFile, middleware.settingsIndex ? this._clientOptions!.middlewareSettings?.[middleware.settingsIndex] : undefined);
           }
         
         if (wpFile.shouldCommit) pagesToEdit.set(wpFile.path, wpFile);
@@ -151,7 +160,7 @@ export class Client extends Evt<
         if (!(md5Hashes.get(file.path) && (md5Hash(file.source.trimEnd()) === md5Hashes.get(file.path))))
           allEdits.push(new Promise((resolve) => {
             setTimeout(() => {
-              this.mwnClient!.edit(file.path, (revision) => {
+              this._mwnClient!.edit(file.path, (revision) => {
                 if (!(md5Hashes.get(file.path) && md5Hash(revision.content.trimEnd()) === md5Hash(file.source.trimEnd()))) {
                   newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
                   return {
@@ -164,7 +173,7 @@ export class Client extends Evt<
               .catch((error) => {
                 if (error.code === "missingtitle") {
                   newMd5Hashes.set(file.path, md5Hash(file.source.trimEnd()));
-                  this.mwnClient!.create(file.path, file.source, file.commitComment).then(resolve).catch((error) => {
+                  this._mwnClient!.create(file.path, file.source, file.commitComment).then(resolve).catch((error) => {
                     this.post(["createError", file, error]);
                   });
                 } else this.post(["editError", file, error])
@@ -178,8 +187,8 @@ export class Client extends Evt<
             newMd5HashesJSON[key] = value;
           });
     
-          fs.writeFileSync(this.clientOptions!.cacheFile, JSON.stringify(newMd5HashesJSON));
-          this.running = false;
+          fs.writeFileSync(this._clientOptions!.cacheFile, JSON.stringify(newMd5HashesJSON));
+          this._running = false;
           this.post(["runningEnded", undefined]);
           resolve(); 
         });
