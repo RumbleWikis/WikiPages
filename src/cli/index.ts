017ddc815f91e5699ec4d67c6151c1764909ff62
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-09-03 14:30:09 -0700

    add succeeded

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -127,7 +127,7 @@ const argv = yargs(process.argv.slice(2))
               });
               try {
                 await client.buildFile(wpFile);
-                console.log(`File ${file} had ${wpFile.errors.length} errors`);
+                console.log(`File ${file} had ${wpFile.errors.length} errors, succeeded: ${wpFile.shouldCommit}`);
                 if (wpFile.errors.length && (argv.silent === "false")) wpFile.errors.forEach(error => console.error(error));
               } catch {
                 if (argv.silent === "false") console.error(`An error occurred while building "${file}"`);
